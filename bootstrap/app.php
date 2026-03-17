2ea383f4da88360047b483c1d07159b31a86ec43
Gabriel <gabrielgvo16@gmail.com>
2026-03-17 09:14:48 -0300

    fix: add https em providers e modificado bootstrap/app para aceitar todos as requsicoes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+    })->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
